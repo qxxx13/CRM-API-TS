0bec6dd2d4e25466f77e50646b3fad2b8865ac2d
Nikolay Nazarenko <redecv9@gmail.com>
2024-06-16 23:17:40 +0300

    add: cert

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 declare const module: any;
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
+    const keyFile = fs.readFileSync('cert/key.pem');
+    const certFile = fs.readFileSync('cert/cert.pem');
+
+    const app = await NestFactory.create(AppModule, {
+        cors: true,
+        httpsOptions: {
+            key: keyFile,
+            cert: certFile,
+        },
+    });
     app.setGlobalPrefix('api');
     await app.listen(5555);
 
