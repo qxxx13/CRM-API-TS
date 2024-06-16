978b5040886312dde31d61459920c2ce96e68ae1
Nikolay Nazarenko <redecv9@gmail.com>
2024-06-17 01:56:59 +0300

    test: ssl

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
 
 declare const module: any;
 
 async function bootstrap() {
-    const keyFile = fs.readFileSync('cert/key.pem');
-    const certFile = fs.readFileSync('cert/cert.pem');
-
     const app = await NestFactory.create(AppModule, {
         cors: true,
-        httpsOptions: {
-            key: keyFile,
-            cert: certFile,
-        },
     });
+
     app.setGlobalPrefix('api');
     await app.listen(5555);
 
