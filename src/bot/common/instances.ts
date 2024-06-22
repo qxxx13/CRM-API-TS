c92af6a915d4a09dc2b7545a3158f9c501780e12
Nikolay Nazarenko <redecv9@gmail.com>
2024-06-22 11:31:46 +0300

    fix: url

--- src/bot/common/instances.ts
+++ src/bot/common/instances.ts
@@ -4,7 +4,7 @@ export const serverInstance = axios.create({
     baseURL: 'http://77.91.84.85:5555/api/',
 });
 
-export const clientInstance = 'http://77.91.84.85:3000/';
+export const clientInstance = 'http://77.91.84.85:3000';
 
 //? PROD: baseURL: 'http://77.91.84.85:5555/api/',  clientInstance = 'http://77.91.84.85';
 //! DEV: baseURL: 'http://localhost:5555/api/', clientInstance = 'http://127.0.0.1:3000';
